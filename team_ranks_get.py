4854a13dccd3e1f8a02c5d21e71c9f9a1bfad61a
Kevin Pei <kevin.pei@carleton.ca>
2017-11-13 11:40:20 -0500

    updated rankings teams

--- team_ranks_get.py
+++ team_ranks_get.py
@@ -3,6 +3,6 @@ from helper import get_existing_data, tabulate, scrape
 
 teamIds = get_existing_data("teams", 2)
 #teamIds = [6301]
-teams = scrape(teamIds, get_teams, 8)
+teams = scrape(teamIds, get_teams, 16)
 
 tabulate("teams_w_ranking", teams)
